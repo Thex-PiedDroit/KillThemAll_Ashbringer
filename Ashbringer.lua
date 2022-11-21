92653f8c8978649077ee16de9e25e681fa5016b2
Thex <thex-pieddroit@hotmail.com>
2022-11-21 18:42:04 +0100

    First commit

---
+++ Ashbringer.lua
@@ -0,0 +1,16 @@
+
+Cerberus_HookThisFile();
+
+g_ashBringer = g_soundLibrary:Inherit({ m_sDisplayName = "Corrupted Ashbringer", m_sDataName = "Ashbringer" });
+g_ashBringer.m_generalSoundFilesList =
+{
+	544421,--"Sound/creature/ashbringer/ash_speak_01.ogg",	-- I was pure once
+	544418,--"Sound/creature/ashbringer/ash_speak_04.ogg",	-- Betrayed by my order
+	544426,--"Sound/creature/ashbringer/ash_speak_06.ogg",	-- Made to serve
+	544427,--"Sound/creature/ashbringer/ash_speak_07.ogg",	-- My son watched me die
+	544428,--"Sound/creature/ashbringer/ash_speak_08.ogg",	-- Crusades fed his rage
+	544428,--"Sound/creature/ashbringer/ash_speak_09.ogg",	-- Truth is unknown to him
+	544419,--"Sound/creature/ashbringer/ash_speak_10.ogg",	-- Scarlet crusade is pure no longer
+	544422,--"Sound/creature/ashbringer/ash_speak_11.ogg",	-- Balnazzar's crusade corrupted my son
+	544417,--"Sound/creature/ashbringer/ash_speak_12.ogg",	-- Kill them all
+};
